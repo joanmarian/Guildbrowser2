16c60e3612c435426fa238c745e23f7ca85cc646
Joan Friedman <joan.friedman@pega.com>
2013-05-30 16:40:21 -0400

    Add wowutils unit tests.

---
+++ GuildBrowserLogicTests/WowUtilsTests.h
@@ -0,0 +1,13 @@
+//
+//  WowUtilsTests.h
+//  GuildBrowser
+//
+//  Created by Joan Friedman on 5/30/13.
+//  Copyright (c) 2013 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface WowUtilsTests : SenTestCase
+
+@end
