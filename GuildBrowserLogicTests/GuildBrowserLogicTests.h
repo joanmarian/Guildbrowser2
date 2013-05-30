f572075f8a97ed98a88a0d72748300f00c92ac11
Joan Friedman <joan.friedman@pega.com>
2013-05-30 17:22:33 -0400

    update scheme

---
+++ GuildBrowserLogicTests/GuildBrowserLogicTests.h
@@ -0,0 +1,13 @@
+//
+//  GuildBrowserLogicTests.h
+//  GuildBrowserLogicTests
+//
+//  Created by Joan Friedman on 5/30/13.
+//  Copyright (c) 2013 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface GuildBrowserLogicTests : SenTestCase
+
+@end
---
+++ GuildBrowserLogicTests/GuildBrowserLogicTests.m
@@ -0,0 +1,32 @@
+//
+//  GuildBrowserLogicTests.m
+//  GuildBrowserLogicTests
+//
+//  Created by Joan Friedman on 5/30/13.
+//  Copyright (c) 2013 Charlie Fulton. All rights reserved.
+//
+
+#import "GuildBrowserLogicTests.h"
+
+@implementation GuildBrowserLogicTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in GuildBrowserLogicTests");
+}
+
+@end
